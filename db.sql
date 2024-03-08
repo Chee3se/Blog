ec417d49f331a2ac22f8bda5e5e0762fe0d5ef77
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-08 11:02:07 +0200

    db

--- db.sql
+++ db.sql
@@ -10,3 +10,29 @@ INSERT INTO posts(title)
 VALUES
 ('My First Blog Post'),
 ('My Second Blog Post');
+
+CREATE TABLE categories (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+INSERT INTO categories (name, description)
+VALUES
+("sport",""),
+("music",""),
+("food","");
+
+ALTER TABLE posts
+ADD category_id INT NOT NULL;
+
+UPDATE posts
+SET category_id = 1
+WHERE id = 1;
+
+UPDATE posts
+SET category_id = 3
+WHERE id = 2;
+
+ALTER TABLE posts
+ADD FOREIGN KEY (category_id) REFERENCES categories(id);
