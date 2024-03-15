31e86136635c6aa4bf99d553c4ef2ba4306704fb
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-15 12:27:15 +0200

    routes upgrade

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
 
-$url = parse_url($_SERVER['REQUEST_URI'])["path"];
-$dir = 'controllers/';
-
-switch ($url) {
-    case '':
-    case '/':
-        require $dir.'index.php';
-        break;
-
-    case '/about':
-        require $dir.'about.php';
-        break;
-
-    case '/story':
-        require $dir.'story.php';
-        break;
-
-    default:
-        http_response_code(404);
-        require $dir.'404.php';
-}
+require 'router.php';
---
+++ router.php
@@ -0,0 +1,9 @@
+<?php
+$routes = require 'routes.php';
+$url = parse_url($_SERVER['REQUEST_URI'])["path"];
+if (array_key_exists($url, $routes)) {
+    require $routes[$url];
+} else {
+    http_response_code(404);
+    require $dir.'404.php';
+}
